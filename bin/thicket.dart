a85e149b9f8676659185ae132ab779697c1a85c7
Scott Hatfield <contact@hatfieldscott.com>
2026-08-03 19:28:51 -0500

    Created Dart CLI from template
    Created a Dart CLI from the default `dart create` template.

---
+++ bin/thicket.dart
@@ -0,0 +1,5 @@
+import 'package:thicket/thicket.dart' as thicket;
+
+void main(List<String> arguments) {
+  print('Hello world: ${thicket.calculate()}!');
+}
---
+++ test/thicket_test.dart
@@ -0,0 +1,8 @@
+import 'package:thicket/thicket.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
